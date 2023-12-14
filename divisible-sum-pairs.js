4b2c2e6ceb3051b1e39a56d3ee6ed74c915c6d36
Kavyanjali Banan <107024314+Kavya6602@users.noreply.github.com>
2023-12-14 00:15:14 -0800

    Update divisible-sum-pairs.js

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,14 +1,38 @@
-let k = 5;
-let ar = [1, 2, 3, 4, 5, 6];
-let n = ar.length;
-let count = 0
-
-for (let i = 0; i < n - 1; i++) {
-    for (let j = i + 1; j < n; j++) {
-        if ((ar[i] + ar[j]) % k === 0) {
-            count++;
-        }
-    }
+function getTotalX(a, b) {
+function gcd(x, y) {
+  while (y !== 0) {
+    const temp = y;
+    y = x % y;
+    x = temp;
+  }
+  return x;
 }
 
-return count;
+function lcm(x, y) {
+  return (x * y) / gcd(x, y);
+}
+
+  // Find the LCM of all elements in array a
+  let lcm_a = a[0];
+  for (let i = 1; i < a.length; i++) {
+    lcm_a = lcm(lcm_a, a[i]);
+  }
+
+  // Find the GCD of all elements in array b
+  let gcd_b = b[0];
+  for (let i = 1; i < b.length; i++) {
+    gcd_b = gcd(gcd_b, b[i]);
+  }
+
+  // Count the multiples of lcm_a that divide gcd_b evenly
+  let count = 0; 
+  let multiple = lcm_a;
+  while (multiple <= gcd_b) {
+    if (gcd_b % multiple === 0) {
+      count += 1;
+    }
+    multiple += lcm_a;
+  }
+
+  return count;
+}
