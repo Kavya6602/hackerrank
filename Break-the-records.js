1a557f4bf6b9b1e0a68dda50c61cc3a8ab5f2c69
Kavyanjali Banan <107024314+Kavya6602@users.noreply.github.com>
2023-12-14 00:10:08 -0800

    Add files via upload

---
+++ Break-the-records.js
@@ -0,0 +1,19 @@
+// let scores = [10,5,20,20,4,5,2,25,1]
+// let max = 0;
+// let maxcount = 0;
+// let min = 0;
+// let mincount = 0;
+
+// for(let i = 0; i < scores.length; i++){
+//     if(max < scores[i]){
+//         max = scores[i];
+//          maxcount++;
+//          console.log(max)
+//     }
+//     if(min > scores[i]){
+//         mincount++;
+//     }
+// }
+
+// console.log([max,min])
+
